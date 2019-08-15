41fe2b6703965bd3ddce82e493b047e7eb6399c9
Mars Le <hp.mars.le@gmail.com>
2019-08-15 18:07:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.size > 2 
   # adds commas plus a final 'and' when given a 3-element array 1, 2, and 3 
-    oxford_string = array.slice().join(", ") #=>  array.split  can i shovel array[i] to a string. yes 
+    oxford_string = array.slice(0,array.length-1).join(", ") #=>  array.split  can i shovel array[i] to a string. yes 
     << " and " + array[-1]
   #oxford_comma correctly formats arrays of lengths greater than three
   else 
