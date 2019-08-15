f6d7252a1038c615819c9f63fe01a3177dd38544
Mars Le <hp.mars.le@gmail.com>
2019-08-15 18:16:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
     #adds 'and' b/t elements when given a 2 element array
     array.join(" and ")
   elsif array.size > 2 
-      array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
+    array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
   else 
     array.join
   end 
