655065ede798e7b547d2edfe60a584911a5ed0ed
Mars Le <hp.mars.le@gmail.com>
2019-08-15 18:21:08 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -9,14 +9,14 @@ def oxford_comma(array)
   #   array.join 
   # end 
   
-  # if array.size == 2 
-  #   #adds 'and' b/t elements when given a 2 element array
-  #   array.join(" and ")
-  # elsif array.size > 2 
-  #   array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
-  # else 
-  #   array.join
-  # end 
+  if array.size == 2 
+    #adds 'and' b/t elements when given a 2 element array
+    array.join(" and ")
+  elsif array.size > 2 
+    array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
+  else 
+    array.join
+  end 
     
   
 end
