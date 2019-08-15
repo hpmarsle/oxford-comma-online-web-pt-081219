76970481d1b3b90287be86c69e97a3d504c0d01c
Mars Le <hp.mars.le@gmail.com>
2019-08-15 18:20:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,13 @@
 def oxford_comma(array)
-  case array.size 
-  when 2
-    array.join(" and ")
-  when > 2 
-    array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
-  else
-    array.join 
-  end 
+  #DOES NOT WORK..ASK JAKE WHY ON caller
+  # case array.size 
+  # when 2
+  #   array.join(" and ")
+  # when > 2 
+  #   array.slice(0,array.length-1).join(", ") << ", and " + array[-1]
+  # else
+  #   array.join 
+  # end 
   
   # if array.size == 2 
   #   #adds 'and' b/t elements when given a 2 element array
